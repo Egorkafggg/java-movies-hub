b605f09ca2928d8a0f424e416453c26c8e2975e9
Егорка <159724676+Egorkafggg@users.noreply.github.com>
2026-03-23 17:56:14 +0300

    Add files via upload

---
+++ 9/src/test/ru/practicum/moviehub/http/ListOfMoviesTypeToken.java
@@ -0,0 +1,8 @@
+package ru.practicum.moviehub.http;
+
+import com.google.gson.reflect.TypeToken;
+import ru.practicum.moviehub.store.Movie;
+import java.util.List;
+
+public class ListOfMoviesTypeToken extends TypeToken<List<Movie>> {
+}

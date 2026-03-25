13772d5873a5f125516551369a2a2a0623924b8e
Егорка <159724676+Egorkafggg@users.noreply.github.com>
2026-03-25 06:56:01 +0300

    Add files via upload

---
+++ sprint9-main/src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -0,0 +1,23 @@
+package ru.practicum.moviehub;
+
+import ru.practicum.moviehub.http.MoviesServer;
+import ru.practicum.moviehub.store.MoviesStore;
+
+public class MovieHubApp {
+    public MovieHubApp() {
+    }
+
+    public static void main(String[] args) {
+        try {
+            MoviesServer serv = new MoviesServer(new MoviesStore(), 8080);
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                //System.out.println("Shutdown hook triggered...");
+                serv.stop();
+            }));
+        } catch (Exception e) {
+            System.err.println("Failed to start server: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+}
